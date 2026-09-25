10b3ac47a6b2023de23c8e24152f9421ccbce244
Anant Maurya <167705402+mauryaanant@users.noreply.github.com>
2026-09-25 15:52:23 +0530

    Time: 0 ms (100%), Space: 9 MB (91.66%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,51 +1,16 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        string ans = "";
+        const int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        const string symbols[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
-        while (num != 0) {
-            if (num >= 1000) {
-                ans += 'M';
-                num -= 1000;
-            } else if (num >= 900) {
-                ans += "CM";
-                num -= 900;
-            } else if (num >= 500) {
-                ans += 'D';
-                num -= 500;
-            } else if (num >= 400) {
-                ans += "CD";
-                num -= 400;
-            } else if (num >= 100) {
-                ans += 'C';
-                num -= 100;
-            } else if (num >= 90) {
-                ans += "XC";
-                num -= 90;
-            } else if (num >= 50) {
-                ans += 'L';
-                num -= 50;
-            } else if (num >= 40) {
-                ans += "XL";
-                num -= 40;
-            } else if (num >= 10) {
-                ans += 'X';
-                num -= 10;
-            } else if (num >= 9) {
-                ans += "IX";
-                num -= 9;
-            } else if (num >= 5) {
-                ans += 'V';
-                num -= 5;
-            } else if (num >= 4) {
-                ans += "IV";
-                num -= 4;
-            } else if (num >= 1) {
-                ans += 'I';
-                num -= 1;
+        string ans = "";
+        for (int i = 0; i < 13 && num > 0; ++i) {
+            while (num >= values[i]) {
+                ans += symbols[i];
+                num -= values[i];
             }
         }
-
         return ans;
     }
 };
